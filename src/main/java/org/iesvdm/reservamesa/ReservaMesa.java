20ef81de4d0d48d561089dfd5d40967940470a9e
mortala <mortala582@g.educaand.es>
2024-05-15 12:08:59 +0200

    fin

--- src/main/java/org/iesvdm/reservamesa/ReservaMesa.java
+++ src/main/java/org/iesvdm/reservamesa/ReservaMesa.java
@@ -110,15 +110,15 @@ public class ReservaMesa {
             {
                 mesaSalida = iIzq;
                 noEncontrada = false;
-                iIzq--;
-            }
 
+            }
+            iIzq--;
             if (noEncontrada && iDer < array.length && array[iDer] + numPersonas <= tamanioMesa) {
                 mesaSalida = iDer;
                 noEncontrada = false;
-                iDer++;
-            }
 
+            }
+            iDer++;
         }
 
         return mesaSalida;
--- src/test/java/org/iesvdm/reservamesa/ReservaMesaTest.java
+++ src/test/java/org/iesvdm/reservamesa/ReservaMesaTest.java
@@ -5,10 +5,115 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ReservaMesaTest {
+    private int[] array;
+
 
     @Test
-    void failTest() {
-        fail("TEST SIN IMPLEMENTAR");
-    }
+    void rellenarMesaTest() {
+    //debo probar que no meta mas de 4 en mesa y que sea mayor que 0
+        int menor = 0;
+        int mayor = 4;
+        ReservaMesa reservaMesa = new ReservaMesa();
 
+        reservaMesa.setTamanioMesa(4);
+
+        int[] mesas = new int[5];
+        reservaMesa.setMesas(mesas);
+
+        reservaMesa.rellenarMesas();
+        for (int i = 0; i < mesas.length; i++) {
+            assertThat(mesas[i]).isGreaterThan(menor);
+            assertThat(mesas[i]).isLessThanOrEqualTo(mayor);
+        }
+
+
+    }
+    @Test
+    void buscarPrimeraMesaVaciaTest(){
+        ReservaMesa reservaMesa = new ReservaMesa();
+        int[] mesas = new int[3];
+        mesas[0] = 1;
+        mesas[1] = 0;
+        mesas[2] = 3;
+        reservaMesa.setMesas(mesas);
+        int mesaVacia=reservaMesa.buscarPrimeraMesaVacia();
+
+
+        assertThat(mesaVacia).isEqualTo(1);
+
+    }
+    @Test
+    void buscarMesaParaCompartirTest(){
+        ReservaMesa reservaMesa = new ReservaMesa();
+        reservaMesa.setTamanioMesa(4);
+        int[] mesas = new int[3];
+        mesas[0] = 1;
+        mesas[1] = 0;
+        mesas[2] = 3;
+        reservaMesa.setMesas(mesas);
+        int mesaCompartir=reservaMesa.buscarMesaParaCompartir(3);
+        assertThat(mesaCompartir).isEqualTo(0);
+
+    }
+    @Test
+    void buscarMesaCompartirMasCercaDeTest(){
+        ReservaMesa reservaMesa = new ReservaMesa();
+        reservaMesa.setTamanioMesa(4);
+        int[] mesas = new int[4];
+        mesas[0] = 1;
+        mesas[1] = 0;
+        mesas[2] = 3;
+        mesas[3] = 1;
+        reservaMesa.setMesas(mesas);
+        int mesaCercana=reservaMesa.buscarMesaCompartirMasCercaDe(2, 2);
+        assertThat(mesaCercana).isEqualTo(1);
+
+    }
+    @Test
+    void buscarMesaCompartirMasAlejadaDeTest(){
+        ReservaMesa reservaMesa = new ReservaMesa();
+        reservaMesa.setTamanioMesa(4);
+        int[] mesas = new int[4];
+        mesas[0] = 1;
+        mesas[1] = 0;
+        mesas[2] = 3;
+        mesas[3] = 1;
+        reservaMesa.setMesas(mesas);
+        int mesaLejos=reservaMesa.buscarMesaCompartirMasAlejadaDe(2, 3);
+        assertThat(mesaLejos).isEqualTo(0);
+    }
+    @Test
+    void buscarCompartirNMesasConsecutivas(){
+
+        // falla, siempre devuelve 0
+
+        ReservaMesa reservaMesa = new ReservaMesa();
+        reservaMesa.setTamanioMesa(4);
+        int[] mesas = new int[4];
+        mesas[0] = 1;
+        mesas[1] = 0;
+        mesas[2] = 3;
+        mesas[3] = 1;
+        reservaMesa.setMesas(mesas);
+
+       int mesasalida=reservaMesa.buscarCompartirNMesasConsecutivas(2,4);
+       assertThat(mesasalida).isEqualTo(1);
+
+
+    }
+    @Test
+    void comensalesTotalesTest(){
+        ReservaMesa reservaMesa = new ReservaMesa();
+        reservaMesa.setTamanioMesa(4);
+        int[] mesas = new int[4];
+        mesas[0] = 1;
+        mesas[1] = 0;
+        mesas[2] = 3;
+        mesas[3] = 1;
+        reservaMesa.setMesas(mesas);
+        int mesaTotales=reservaMesa.comensalesTotales();
+        assertThat(mesaTotales).isEqualTo(5);
+
+
+    }
 }
